ebb24e2ba62de6e02cc0eea39a2385be34c52ccf
skyxgods <31897040+k0d31@users.noreply.github.com>
2025-07-18 14:33:28 +0200

    config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Загружает .env файл
+load_dotenv()
 
 config = {
     "token": os.getenv("TOKEN"),
+    "owner_id": int(os.getenv("OWNER_ID", 0))
 }
